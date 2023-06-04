c49ae64c9ab62ba15bb90c6209c6e136dc2db3d1
tamagoez <87574268+tamagoez@users.noreply.github.com>
2023-06-04 15:51:55 +0000

    Add debug mode

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
-const withPWA = require('next-pwa')({
+const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  })
-  
-  module.exports = withPWA({
-    // next.js config
-  })
+});
+
+module.exports = withPWA({
+  // 開発環境での設定
+  devIndicators: {
+    autoPrerender: false, // プリレンダリングの自動化を無効にする
+  },
+  minify: false, // minify（圧縮）を無効にする
+});
