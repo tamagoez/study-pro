137e1d705481da58667a2c099231b6544186544a
tamagoez <87574268+tamagoez@users.noreply.github.com>
2023-07-06 16:57:52 +0000

    [wb] fix relation 2

--- scripts/workbook/section/page/page.ts
+++ scripts/workbook/section/page/page.ts
@@ -20,7 +20,7 @@ export async function createPage(
       ownerid: userid,
       sectionid,
     })
-    .select("url, id")
+    .select("id")
     .single();
   if (error) {
     console.error(error);
